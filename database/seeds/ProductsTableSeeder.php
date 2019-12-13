2ae36d0af19f5ebb2d7d32ba1b9456a6b4baf122
patriotHunter <yoshua93@outlook.com>
2019-12-13 18:45:50 +0000

    inserting products

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -15,17 +15,87 @@ class ProductsTableSeeder extends Seeder
                         [
                             'id' => 1,
                             'product' => 'Witcher 3 - Wild Hunt',
-                            'status' => 1,
-                            'price' => 1,
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
 
                         [
                             'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
+                            'product' => 'Red Dead Redemption II',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'Halo: The Master Chief Collection',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'Grand Theft Auto V',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'Bioshock Infinite',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'Half-Life: Alyx ',
+                            'status' => 'pre order',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'Dirt 3',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'Dirt 4',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'DOOM',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'Call of Duty Modern Warfare',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'FIFA 20',
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
@@ -33,8 +103,15 @@ class ProductsTableSeeder extends Seeder
                         [
                             'id' => 1,
                             'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+                        
+                        [
+                            'id' => 1,
+                            'product' => 'Forza 4 Horizon',
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
@@ -42,135 +119,190 @@ class ProductsTableSeeder extends Seeder
                         [
                             'id' => 1,
                             'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
                         
                         [
                             'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
+                            'product' => 'Star Wars: Jedi Fallen Order ',
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
                         
                         [
                             'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
+                            'product' => 'Need for Speed Heat',
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
                         
                         [
                             'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
+                            'product' => 'Boorderlands 3',
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
                         
                         [
                             'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
+                            'product' => 'Spec Ops: The Line',
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
                         
                         [
                             'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
+                            'product' => 'Max Payne 3',
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
                         
                         [
                             'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
-                            'created_at' => 1,
-                            'updated_at' => 1
-                        ],[
-                            'id' => 1,
-                            'product' => 1,
-                            'rating' => 1,
-                            'price' => 1,
+                            'product' => 'Resident Evil 2',
+                            'status' => 'avaible',
                             'created_at' => 1,
                             'updated_at' => 1
                         ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Overwatch',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Assassins Creed Odyssey',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Watch Dogs Legion',
+                            'status' => 'pre order',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'DOOM Eternal',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Ace Combat 7: Skies Unknown',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Destroy All Humans',
+                            'status' => 'pre order',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Beyond Good and Evil 2',
+                            'status' => 'soon',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Alien Isolation',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Alien Isolation',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Gran Turismo Sport',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Firewatch',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Call of Duty Black Ops 3',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Call of Duty Black Ops 4',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Call of Duty Black Ops 4',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Rocket League',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+                        [
+                            'id' => 1,
+                            'product' => 'Counter Strike: Global Offensive',
+                            'status' => 'avaible',
+                            'created_at' => 1,
+                            'updated_at' => 1
+                        ],
+
+
+
+
                     ];
 
         $products_genres = [
