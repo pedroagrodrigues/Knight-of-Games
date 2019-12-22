bfb0c519b0e49dd5f0d0c7fe761247d47ee821a6
patriotHunter <yoshua93@outlook.com>
2019-12-22 19:43:30 +0000

    continuation

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UsersTableSeeder::class);
-        //$this->call(SocialsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(SocialsTableSeeder::class);
         $this->call(CompaniesTableSeeder::class);
         $this->call(GenresTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
