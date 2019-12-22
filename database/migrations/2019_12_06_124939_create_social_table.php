678603f6bdafcf5060d1903c61aaddd950a58b39
patriotHunter <yoshua93@outlook.com>
2019-12-22 15:45:51 +0000

    change to plural socials to prevent errors in the SQL injection

--- database/migrations/2019_12_06_124939_create_social_table.php
+++ database/migrations/2019_12_06_124939_create_social_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSocialTable extends Migration
+class CreateSocialsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSocialTable extends Migration
      */
     public function up()
     {
-        Schema::create('social', function (Blueprint $table) {
+        Schema::create('socials', function (Blueprint $table) {
             $table->increments('id');
             $table->enum('company', ['Google','Facebook','Twitter']);
             $table->timestamps();
