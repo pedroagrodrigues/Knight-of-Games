d13009947c28225028db73d44c2a309fa3051a35
patriotHunter <yoshua93@outlook.com>
2019-12-25 01:22:11 +0000

    something to see for the product - companies relationship

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,10 @@ class ProductController extends Controller
     public function viewProduct()
     {
         $products = Product::where('id', $id)->get(); //finding the specific game
-        //$product_companies = 
+        // SELECT companies.company FROM products, product_has_companies, companies 
+        //     where products.id = 1 AND products.id = product_has_companies.product_id and companies.id = product_has_companies.company_id
+        // $companies = Product::where('id', $id)
+        //->with('companies')->get();
 
         return view('product', ['products' => $products]);
     }
