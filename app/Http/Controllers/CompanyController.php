70b8b1fa52622e6da69b0694e040d30e0d266b48
patriotHunter <yoshua93@outlook.com>
2019-12-26 20:11:40 +0000

    implementing insert company

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -21,12 +21,35 @@ class CompanyController extends Controller
 
     public function viewCompany($id)
     {
-        $companies = DB::table('companies')->where('id', $id)->get();
+        //$companies = DB::table('companies')->where('id', $id)->get();
+        $companies = Company::where('id', $id)->get();
         return view('company', ['companies' => $companies]);
     }
 
-    public function createCompany(Request $request_name, Request $request_description, Request $request_time_at)
+    public function createCompany(Request $request_name, Request $request_description, Request $request_created)
     {
+        $date = date('Y-m-d H:i:s');
+        $company_name           = $request_name->input('company_name');
+        $company_description    = $request_description->input('company_description');
+        $company_created        = $request_created->input('company_created');
 
+        if ($company_created == NULL)
+        {
+            $company = Company::create([
+                'company'       => $company_name,
+                'description'   => $company_description,
+                'created_at'    => $date,
+                'updated_at'    => $date
+            ]);
+        }
+        else 
+        {
+            $company = Company::create([
+                'company'       => $company_name,
+                'description'   => $company_description,
+                'created_at'    => $company_created,
+                'updated_at'    => $date
+            ]);
+        }
     }
 }
