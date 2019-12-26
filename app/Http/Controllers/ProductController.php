fc5a4d527fb62d6c984086feca87c3e1970c1748
patriotHunter <yoshua93@outlook.com>
2019-12-26 17:06:12 +0000

    inserting future values

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,19 +13,24 @@ class ProductController extends Controller
         return view('products', ['products' => $products]);
     }
 
-    public function viewProduct()
+    public function viewProduct($id)
     {
         $products = Product::where('id', $id)->get(); //finding the specific game
         // SELECT companies.company FROM products, product_has_companies, companies 
         //     where products.id = 1 AND products.id = product_has_companies.product_id and companies.id = product_has_companies.company_id
-        // $companies = Product::where('id', $id)
+        $companies = Product::with('companies')->where('id', $id)->get();
         //->with('companies')->get();
 
-        return view('product', ['products' => $products]);
+        return view('product', ['products' => $products, 'companies' => $companies]);
     }
 
-    public function createNewProduct()
+    public function createNewProduct(   Request $request_name, 
+                                        Request $request_description,
+                                        Request $request_companies,
+                                        Request $request_genres,
+                                        Request $request_sites,
+                                        Request $request_release)
     {
-        
+                
     }
 }
