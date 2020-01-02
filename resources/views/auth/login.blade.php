6b487d9c920d0fddad6c1e58873e0305d3cb8b93
Pedro Rodrigues <pedroagrodrigues@github.com>
2020-01-02 00:49:02 +0000

    tentativa de colocar o logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,12 @@
 @section('content')
 <link href="{{ asset('css/account.css') }}" rel="stylesheet">
 <div class='authentication-wrapper'>
-    <!-- <a href="{{ url('/') }}">
-        <div class="logo"></div>
-    </a> -->
+    <a href="{{ url('/') }}">
+        <div class='logo-wrapper'>
+            <div class='logo-base'></div>
+            <div class="logo">teste</div>
+        </div>
+    </a>
     <form action="{{ route('login') }}" class="form-authentication" method="POST">
         @csrf
         <div class="form-inputs">
